c848418d12db0699bc6f5f4d8b85bddffd8c4854
Jordan Knight <jakkaj@gmail.com>
2017-05-23 14:01:24 +1000

    Started project, ava running

---
+++ node/func/build/output_test/runTests/testCosmosGraph.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ava_1 = require("ava");
+console.log("testing 123");
+ava_1.default('test', t => {
+    console.log("hi");
+    t.pass();
+});
+//# sourceMappingURL=testCosmosGraph.js.map
---
+++ node/func/test/src/runTests/testCosmosGraph.ts
@@ -0,0 +1,8 @@
+import test from 'ava';
+
+console.log("testing 123");
+
+test('test', t=>{
+    console.log("hi");
+    t.pass();
+})
